b4020b30453f3e1c60e625fd7d5e53df25de5581
Vito Van <vito@sdf.org>
2022-01-12 15:06:49 +0800

    Add MacOS (darwin) compatibility

--- src/library.lisp
+++ src/library.lisp
@@ -2,6 +2,7 @@
 
 #-:gamekit
 (cffi:define-foreign-library libsdl2-gfx
+  (:darwin (:or "libSDL2_gfx-1.0.0.dylib" "libSDL2_gfx.dylib"))
   (:unix "libSDL2_gfx-1.0.so.0")
   (:windows "SDL2_gfx.dll")
   (t (:default "libSDL2_gfx")))
